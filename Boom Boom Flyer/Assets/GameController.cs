383b0734503f0287993b5fe10bd9ff12b71bbdae
DESKTOP-NDUKOTR\Peder <peder@zuricho.dk>
2017-11-19 04:51:40 +0100

    Peder Unity latest update

---
+++ Boom Boom Flyer/Assets/GameController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+    private GameObject[] playerArr = new GameObject[4];
+    private int numberOfDeadPlayers = 0;
+    private bool deadRegistered1 = false;
+    private bool deadRegistered2 = false;
+    private bool deadRegistered3 = false;
+    private bool deadRegistered4 = false;
+
+    public GameObject endGameObject;
+
+    // Use this for initialization
+    void Start ()
+    {
+        playerArr = GameObject.FindGameObjectsWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (playerArr[0].GetComponent<Movement2>().dead && !deadRegistered1)
+        {
+            numberOfDeadPlayers += 1;
+            deadRegistered1 = true;
+        }
+        if (playerArr[1].GetComponent<Movement2>().dead && !deadRegistered2)
+        {
+            numberOfDeadPlayers += 1;
+            deadRegistered2 = true;
+        }
+        if (playerArr[2].GetComponent<Movement2>().dead && !deadRegistered3)
+        {
+            numberOfDeadPlayers += 1;
+            deadRegistered3 = true;
+        }
+        if (playerArr[3].GetComponent<Movement2>().dead && !deadRegistered4)
+        {
+            numberOfDeadPlayers += 1;
+            deadRegistered4 = true;
+        }
+
+
+        if(numberOfDeadPlayers >= 3)
+        {
+            Time.timeScale = 0.0f;
+            endGameObject.SetActive(true);
+        }
+
+
+    }
+    public void Replay()
+    {
+        SceneManager.LoadScene("Peder");
+    }
+}
--- Boom Boom Flyer/Assets/Scripts/Movement2.cs
+++ Boom Boom Flyer/Assets/Scripts/Movement2.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement2 : MonoBehaviour {
-
-
+    [SerializeField]
+    private float bombCoolDownTime = 0.5f;
     public float forceUp = 4;
     public float maxFuel = 2.5f;
     private float fuel = 1.5f;
@@ -12,27 +12,32 @@ public class Movement2 : MonoBehaviour {
     public float burnrate = 0.1f;
     private bool playing;
     private float forceStrafe = 2;
-
-
+    [SerializeField]
+    private GameObject bomb;
     public float maxSpeed = 4f;
     private string playerName;
     private string upIn;
     private string leftIn;
     private string rightIn;
+    private string downIn;
     private string[] joystickNameArr = new string[4];
     private GameObject player1;
     private GameObject player2;
     private GameObject player3;
     private GameObject player4;
+    private bool firstframe = true;
+    private float callTime;
+    public bool dead = false;
+
 
     // Use this for initialization
     private void Awake()
     {
+
         playing = true;
+        callTime = Time.time;
     }
-    void Start () {
-        StartCoroutine("Fuel");
-	}
+
     private void OnEnable()
     {
         playerName = gameObject.name;
@@ -44,7 +49,9 @@ public class Movement2 : MonoBehaviour {
             upIn = "X1";
             leftIn = "Left1";
             rightIn = "Right1";
-           
+            downIn = "Bomb1";
+            
+
         }
         if (playerName == "Penis2")
         {
@@ -52,6 +59,7 @@ public class Movement2 : MonoBehaviour {
             upIn = "X2";
             leftIn = "Left2";
             rightIn = "Right2";
+            downIn = "Bomb2";
 
         }
         if (playerName == "Penis3")
@@ -60,6 +68,9 @@ public class Movement2 : MonoBehaviour {
             upIn = "X3";
             leftIn = "Left3";
             rightIn = "Right3";
+            downIn = "Bomb3";
+
+
         }
         if (playerName == "Penis4")
         {
@@ -67,11 +78,67 @@ public class Movement2 : MonoBehaviour {
             upIn = "X4";
             leftIn = "Left4";
             rightIn = "Right4";
+            downIn = "Bomb4";
+
+
         }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    void Start()
+    {
+        StartCoroutine("Fuel");
+      
+
     }
     // Update is called once per frame
     void Update()
     {
+        if (firstframe)
+        {
+
+           /* if (gameObject == player1)
+            {
+
+                Physics2D.IgnoreCollision(player1.GetComponent<Collider2D>(), player2.GetComponent<Collider2D>(), true);
+                Physics2D.IgnoreCollision(player1.GetComponent<Collider2D>(), player3.GetComponent<Collider2D>(), true);
+                Physics2D.IgnoreCollision(player1.GetComponent<Collider2D>(), player4.GetComponent<Collider2D>(), true);
+            }
+            if (gameObject == player2)
+            {
+                Physics2D.IgnoreCollision(player2.GetComponent<Collider2D>(), player1.GetComponent<Collider2D>(), true);
+                Physics2D.IgnoreCollision(player2.GetComponent<Collider2D>(), player3.GetComponent<Collider2D>(), true);
+                Physics2D.IgnoreCollision(player2.GetComponent<Collider2D>(), player4.GetComponent<Collider2D>(), true);
+            }
+
+            if (gameObject == player3)
+            {
+                Physics2D.IgnoreCollision(player3.GetComponent<Collider2D>(), player1.GetComponent<Collider2D>(), true);
+                Physics2D.IgnoreCollision(player3.GetComponent<Collider2D>(), player2.GetComponent<Collider2D>(), true);
+                Physics2D.IgnoreCollision(player3.GetComponent<Collider2D>(), player4.GetComponent<Collider2D>(), true);
+            }
+
+            if (gameObject == player4)
+            {
+                Physics2D.IgnoreCollision(player4.GetComponent<Collider2D>(), player3.GetComponent<Collider2D>(), true);
+                Physics2D.IgnoreCollision(player4.GetComponent<Collider2D>(), player2.GetComponent<Collider2D>(), true);
+                Physics2D.IgnoreCollision(player4.GetComponent<Collider2D>(), player4.GetComponent<Collider2D>(), true);
+            }*/
+            firstframe = false;
+        }
+
+
+
         if (/*Input.GetKey(KeyCode.W) || */Input.GetButton(upIn))
         {
             MoveVertically(upIn);
@@ -108,6 +175,23 @@ public class Movement2 : MonoBehaviour {
         {
             MoveHorrizontally(rightIn);
         }
+
+        if (Input.GetButtonDown(downIn) && !dead)
+        {
+            if (Time.time - callTime > bombCoolDownTime)
+            {
+                callTime = Time.time;
+                DropBomb(downIn);
+            }
+        }
+        else
+        {
+            Debug.Log("bomb is on cooldown");
+        
+        }
+
+
+
         if (Input.GetKey(KeyCode.O))
         {
            // playing = false;
@@ -117,25 +201,26 @@ public class Movement2 : MonoBehaviour {
 private void MoveVertically(string x)
     {
         x = upIn;
-        if(x == "X1")
+        if(x == "X1" && !dead)
         {
             if (fuel > 0)
                 player1.GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * forceUp);
                 player1.GetComponent<Animator>().SetBool("FlyingBool", true);
         }
-        if(x == "X2")
+        if(x == "X2" && !dead)
         {
             if (fuel > 0)
                 player2.GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * forceUp);
                 player2.GetComponent<Animator>().SetBool("Flying2", true);
+
         }
-        if (x == "X3")
+        if (x == "X3" && !dead)
         {
             if (fuel > 0)
                 player3.GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * forceUp);
                 player3.GetComponent<Animator>().SetBool("Flying3", true);
         }
-        if (x == "X4")
+        if (x == "X4" && !dead)
         {
             if (fuel > 0)
                 player4.GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * forceUp);
@@ -147,42 +232,42 @@ private void MoveVertically(string x)
     private void MoveHorrizontally(string x)
     {
         
-        if (x == "Left1")
+        if (x == "Left1" && !dead)
         {
                 if(player1.GetComponent<Rigidbody2D>().velocity.x < maxSpeed && player1.GetComponent<Rigidbody2D>().velocity.x > -maxSpeed)
                 player1.GetComponent<Rigidbody2D>().AddForce(gameObject.transform.right * -forceStrafe);
         }
-        if (x == "Right1")
+        if (x == "Right1" && !dead)
         {
             if (player1.GetComponent<Rigidbody2D>().velocity.x < maxSpeed && player1.GetComponent<Rigidbody2D>().velocity.x > -maxSpeed)
                 player1.GetComponent<Rigidbody2D>().AddForce(gameObject.transform.right * forceStrafe);
         }
-        if (x == "Left2")
+        if (x == "Left2" && !dead)
         {
             if (player2.GetComponent<Rigidbody2D>().velocity.x < maxSpeed && player2.GetComponent<Rigidbody2D>().velocity.x > -maxSpeed)
                 player2.GetComponent<Rigidbody2D>().AddForce(gameObject.transform.right * -forceStrafe);
         }
-        if (x == "Right2")
+        if (x == "Right2" && !dead)
         {
             if (player2.GetComponent<Rigidbody2D>().velocity.x < maxSpeed && player2.GetComponent<Rigidbody2D>().velocity.x > -maxSpeed)
                 player2.GetComponent<Rigidbody2D>().AddForce(gameObject.transform.right * forceStrafe);
         }
-        if (x == "Left3")
+        if (x == "Left3" && !dead)
         {
             if (player3.GetComponent<Rigidbody2D>().velocity.x < maxSpeed && player3.GetComponent<Rigidbody2D>().velocity.x > -maxSpeed)
                 player3.GetComponent<Rigidbody2D>().AddForce(gameObject.transform.right * -forceStrafe);
         }
-        if (x == "Right3")
+        if (x == "Right3" && !dead)
         {
             if (player3.GetComponent<Rigidbody2D>().velocity.x < maxSpeed && player3.GetComponent<Rigidbody2D>().velocity.x > -maxSpeed)
                 player3.GetComponent<Rigidbody2D>().AddForce(gameObject.transform.right * forceStrafe);
         }
-        if (x == "Left4")
+        if (x == "Left4" && !dead)
         {
             if (player4.GetComponent<Rigidbody2D>().velocity.x < maxSpeed && player4.GetComponent<Rigidbody2D>().velocity.x > -maxSpeed)
                 player4.GetComponent<Rigidbody2D>().AddForce(gameObject.transform.right * -forceStrafe);
         }
-        if (x == "Right4")
+        if (x == "Right4" && !dead)
         {
             if (player4.GetComponent<Rigidbody2D>().velocity.x < maxSpeed && player4.GetComponent<Rigidbody2D>().velocity.x > -maxSpeed)
                 player4.GetComponent<Rigidbody2D>().AddForce(gameObject.transform.right * forceStrafe);
@@ -219,4 +304,60 @@ private void MoveVertically(string x)
 
         
     }
+
+
+
+    private void DropBomb(string x)
+    {
+        //Debug.Log(Time.time - callTime);
+
+
+
+        switch (x)
+
+        {
+            case "Bomb1":
+                {
+                    Instantiate(bomb, player1.transform.position - new Vector3(0, 1), Quaternion.identity);
+                    break;
+                }
+
+            case "Bomb2":
+                {
+                    Instantiate(bomb, player2.transform.position - new Vector3(0, 1), Quaternion.identity);
+
+                    break;
+                }
+            case "Bomb3":
+                {
+                    Instantiate(bomb, player3.transform.position - new Vector3(0, 1), Quaternion.identity);
+
+                    break;
+                }
+            case "Bomb4":
+                {
+                    Instantiate(bomb, player4.transform.position - new Vector3(0, 1), Quaternion.identity);
+
+                    break;
+                }
+
+            default: break;
+
+        }
+    
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+            Physics2D.IgnoreCollision(collision.collider, collision.otherCollider);
+        }
+
+
+
+    }
+
+   
+
 }
